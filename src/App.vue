47296620f641fdb619daacec2e524ce9fa0dd09f
apartgecko88165 <86565740+apartgecko88165@users.noreply.github.com>
2022-03-24 08:10:56 -0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script lang="ts">
 import { defineComponent } from "vue";
-
 export default defineComponent({
   data() {
     return {
@@ -10,9 +9,9 @@ export default defineComponent({
   computed: {
     slider() {
       if (this.isChecked) {
-        return "absolute bg-gray-300 shadow-lg border-2 border-green-300 w-[1.75rem] h-[1.75rem] rounded-xl";
+        return "absolute bg-gray-700 shadow-lg border-2 border-green-300 w-[1.75rem] h-[1.75rem] rounded-xl";
       } else {
-        return "absolute bg-gray-300 shadow-lg border-2 border-red-300 w-[1.75rem] h-[1.75rem] rounded-xl";
+        return "absolute bg-gray-700 shadow-lg border-2 border-red-300 w-[1.75rem] h-[1.75rem] rounded-xl";
       }
     },
   },
@@ -20,18 +19,16 @@ export default defineComponent({
 </script>
 
 <template>
-  <div>
-    <span class="flex">
-      <input
-        type="checkbox"
-        class="appearance-none peer bg-gray-200 rounded-xl w-[5rem] h-[2.25rem] border-3 border-gray-500"
-        v-model="isChecked"
-      />
-      <span
-        class="group absolute flex flex-col w-[5rem] h-[2.2rem] p-1 justify-center peer-checked:items-end pointer-events-none"
-      >
-        <span :class="slider" />
-      </span>
+  <span class="flex">
+    <input
+      type="checkbox"
+      class="appearance-none peer bg-gray-600 rounded-xl w-[5rem] h-[2.25rem] border-2 border-gray-500"
+      v-model="isChecked"
+    />
+    <span
+      class="group absolute flex flex-col w-[5rem] h-[2.2rem] p-1 justify-center pointer-events-none transition-transform duration-300 peer-checked:translate-x-11"
+    >
+      <span :class="slider" />
     </span>
-  </div>
+  </span>
 </template>
